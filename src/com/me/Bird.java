d741fc3c425a747c797915837b124339d420e651
patrickatcw <pj8392@yahoo.com>
2018-03-27 15:25:10 -0400

    changes in bird

--- src/com/me/Bird.java
+++ src/com/me/Bird.java
@@ -3,7 +3,10 @@ package com.me;
 //step 14 create bird class
 //step 15 extend
 //step 21 make abstract
-public abstract class Bird extends Animal{
+//public abstract class Bird extends Animal{
+
+//step 39 implement canFly, above original commented out
+public abstract class Bird extends Animal implements CanFly {
 
     //step 16 constructor
     public Bird(String name) {
@@ -25,7 +28,16 @@ public abstract class Bird extends Animal{
     }
 
     //step 22 add another abstract method
-    public abstract void fly();
+    //public abstract void fly();
+    //step 40 get rid of abstract method for fly, above commented out
+    //and instead add override
+    @Override
+    public void fly() {
+        System.out.println(getName() + " is flapping it's wings");
+    }
+
+    //step 41 go to parrot class
+
 
     //step 23 to main, look at error created on bird instance,
     //so create a new class parrot
